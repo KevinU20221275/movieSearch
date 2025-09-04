b41def685a46852563273e1bb0042482398c7c17
KevinU20221275 <U20221275@univo.edu.sv>
2025-09-04 11:27:27 -0600

    add defaults.js to store fallback movie data

---
+++ website/assets/js/defaults.js
@@ -0,0 +1,14 @@
+export const DEFAULT_RESPONSES = {
+    notFound: {
+        title: "Movie not found",
+        year: "Sorry, we couldn't find your movie.",
+        poster: "/assets/img/badMovieRes.png",
+        description: "No results found. Please try again or contact us to request this movie."
+    },
+    error: {
+        title: "Something went wrong",
+        year: "Please try again later.",
+        poster: "/assets/img/badMovieRes.png",
+        description: "If the problem persists, please contact us."
+    }
+};
