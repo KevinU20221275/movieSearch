f414fa2ba824fb25a9286fc34ef040b32515fa66
KevinU20221275 <U20221275@univo.edu.sv>
2025-09-04 11:10:16 -0600

    add /api/movies endpoint to fetch movie data

--- server.js
+++ server.js
@@ -18,6 +18,26 @@ app.use(express.static(path.join(__dirname, 'website')))
 const PORT = process.env.PORT || 3000;
 const API_KEY = process.env.API_KEY
 
+/* Endpoint: This endpoint receives a "search" query parameter
+It makes a request to the OMDb API using the API_KEY
+and returns the movie data as JSON to the client */
+app.get('/api/movies', async (req, res) => {
+    const { search } = req.query
+
+    try {
+        // Request to OMDb API with the movie title
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&t=${search}`) 
+        const data = await response.json();
+
+        // Send successful response to the client
+        res.json(data)
+    } catch (error) {
+        console.error(error)
+        
+        // Send a 500 status response with an error message
+        res.status(500).json({error : "Error fetching movie data"})
+    }
+})
 
 // Start the server
 // The server listens on the port specified in .env or defaults to 3000
