dca84dcc5aa182cc82bbc436edfd09ef7930788b
KevinU20221275 <U20221275@univo.edu.sv>
2025-09-04 11:12:31 -0600

    add helper function to format movie data from API response

--- website/assets/js/app.js
+++ website/assets/js/app.js
@@ -9,3 +9,13 @@ const $searchInput = document.getElementById("search");
 
 // helper variable to store the last searched movie
 let lastSearch = "";
+
+// format API response into movie data
+const formatMovieData = (data) => {
+    return {
+        title : data.Title,
+        year : data.Year,
+        poster : data.Poster !== NULL_FIELD_RESPONSE ? data.Poster : "assets/img/defaultBg.png",
+        description: data.Plot !== NULL_FIELD_RESPONSE ? data.Plot : data.Actors
+    }
+}
